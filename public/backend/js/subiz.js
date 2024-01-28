a540ceea0f1052efcc4df3fe62a2fd672e56fa06
phungquocminhkhanh <khanh.phungquocminh@gmail.com>
2024-01-29 00:21:53 +0700

    x

--- public/backend/js/subiz.js
+++ public/backend/js/subiz.js
@@ -1,10 +1,23 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/658eee668d261e1b5f4d6bbe/1hir56gku';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+// var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/658eee668d261e1b5f4d6bbe/1hir56gku';
+// s1.charset='UTF-8';
+// s1.setAttribute('crossorigin','*');
+// s0.parentNode.insertBefore(s1,s0);
+// })();
 
+(function (a, b, c, d, e, j, s) {
+    a[d] =
+        a[d] ||
+        function () {
+            (a[d].a = a[d].a || []).push(arguments);
+        };
+    (j = b.createElement(c)), (s = b.getElementsByTagName(c)[0]);
+    j.async = true;
+    j.charset = "UTF-8";
+    j.src = "https://static.meiqia.com/widget/loader.js";
+    s.parentNode.insertBefore(j, s);
+})(window, document, "script", "_MEIQIA");
+_MEIQIA("entId", "4f49e20a8d5364a3b7fe7363c06c28f0");
--- resources/views/customer/chat_subiz.blade.php
+++ resources/views/customer/chat_subiz.blade.php
@@ -1,9 +1,42 @@
-<style>
-    #subiz {
-        width: 500px !important;
-    }
-</style>
+<!DOCTYPE html>
+<html lang="en">
 
-<body style="width: 100%;background-color: black">
-    <script src="{{asset('backend/js/subiz.js?v'.env('THEME_VERSION','1'))}}"></script>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible " content="IE=edge" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>CSKH TIKI</title>
+    <style>
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0
+        }
+    </style>
+</head>
+
+<body>
+
+    <script type='text/javascript'>
+        (function(m, ei, q, i, a, j, s) {
+        m[i] = m[i] || function() {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        j = ei.createElement(q),
+            s = ei.getElementsByTagName(q)[0];
+        j.async = true;
+        j.charset = 'UTF-8';
+        j.src = 'https://static.meiqia.com/dist/meiqia.js';
+        s.parentNode.insertBefore(j, s);
+    })(window, document, 'script', '_MEIQIA');
+    _MEIQIA('entId', '4f49e20a8d5364a3b7fe7363c06c28f0');
+    _MEIQIA('language','vi');
+    _MEIQIA('languageLocal','true')
+    </script>
+
 </body>
+
+</html>
